997f78d9547089eb3f4f495c84897c52da61f20a
luanamariadaluzbarbosa@gmail.com <luanamariadaluzbarbosa@gmail.com>
2022-08-19 21:37:45 -0300

    Deploy do Projeto Blog Pessoal

--- blogpessoal/src/test/java/com/generation/blogpessoal/repository/UsuarioRepositoryTest.java
+++ blogpessoal/src/test/java/com/generation/blogpessoal/repository/UsuarioRepositoryTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -15,22 +16,28 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 import com.generation.blogpessoal.model.Usuario;
-@ SpringBootTest ( webEnvironment = WebEnvironment . RANDOM_PORT )
-@ TestInstance ( TestInstance . Lifecycle . PER_CLASS )
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UsuarioRepositoryTest {
 
-	@ Autowired
-	private  UsuarioRepository  usuarioRepository ;
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 	
-	void  start () {
-		
-		usuarioRepository . deleteAll ();
-		usuarioRepository . save ( new  Usuario ( 0L , "João da Silva" , "joao@email.com.br" , "13465278" , "https://i.imgur.com/FETvs20.jpg" ));
-		usuarioRepository . save ( new  Usuario ( 0L , "Manuela da Silva" , "manuela@email.com.br" , "13465278" , "https://i.imgur.com/NtyGneo.jpg" ));
-		usuarioRepository . save ( new  Usuario ( 0L , "Adriana da Silva" , "adriana@email.com.br" , "13465278" , "https://i.imgur.com/mB3YM2N.jpg" ));
-		usuarioRepository . save ( new  Usuario ( 0L , "Paulo Antunes" , "paulo@email.com.br" , "13465278" , "https://i.imgur.com/JR7kUFU.jpg" ));
-		}
-	
+	@BeforeAll
+	void start() {
+
+		usuarioRepository.deleteAll();
+		usuarioRepository.save(
+				new Usuario(0L, "João da Silva", "joao@email.com.br", "13465278", "https://i.imgur.com/FETvs20.jpg"));
+		usuarioRepository.save(new Usuario(0L, "Manuela da Silva", "manuela@email.com.br", "13465278",
+				"https://i.imgur.com/NtyGneo.jpg"));
+		usuarioRepository.save(new Usuario(0L, "Adriana da Silva", "adriana@email.com.br", "13465278",
+				"https://i.imgur.com/mB3YM2N.jpg"));
+		usuarioRepository.save(
+				new Usuario(0L, "Paulo Antunes", "paulo@email.com.br", "13465278", "https://i.imgur.com/JR7kUFU.jpg"));
+	}
+
 	@Test
 	@DisplayName("Retorna 1 usuario")
 	public void deveRetornarUmUsuario() {
@@ -39,7 +46,7 @@ public class UsuarioRepositoryTest {
 
 		assertTrue(usuario.get().getUsuario().equals("joao@email.com.br"));
 	}
-	
+
 	@Test
 	@DisplayName("Retorna 3 usuarios")
 	public void deveRetornarTresUsuarios() {
@@ -47,12 +54,12 @@ public class UsuarioRepositoryTest {
 		List<Usuario> listaDeUsuarios = usuarioRepository.findAllByNomeContainingIgnoreCase("Silva");
 
 		assertEquals(3, listaDeUsuarios.size());
-		
+
 		assertTrue(listaDeUsuarios.get(0).getNome().equals("João da Silva"));
 		assertTrue(listaDeUsuarios.get(1).getNome().equals("Manuela da Silva"));
 		assertTrue(listaDeUsuarios.get(2).getNome().equals("Adriana da Silva"));
 	}
-	
+
 	@AfterAll
 	public void end() {
 		usuarioRepository.deleteAll();
